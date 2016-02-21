3baac547ef7cc8062ca53af2e4da4848d89199fe
Erik Michael Swan <erikmswan@gmail.com>
2016-02-20 16:54:03 -0800

    Redone directory structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,9 @@
 // include gulp
-var gulp = require('gulp'); 
+var gulp = require('gulp');
 
 // include plug-ins
 var jshint = require('gulp-jshint');
 var cache = require('gulp-cached');
-var sass = require('gulp-ruby-sass');
 var changed = require('gulp-changed');
 var imagemin = require('gulp-imagemin');
 var minifyHTML = require('gulp-minify-html');
@@ -15,7 +14,31 @@ var autoprefix = require('gulp-autoprefixer');
 var minifyCSS = require('gulp-minify-css');
 var gzip = require('gulp-gzip');
 var smushit = require('gulp-smushit');
+var vinylfs = require('vinyl-fs');
 
+
+// Directory structure
+var dir = {
+  root: {
+    dist: './dist/',
+    live: './live/'
+  },
+  js: {
+    dist: './dist/js',
+    live: './live/js'
+  },
+  css: {
+    dist: './dist/css',
+    live: './live/css'
+  },
+  img: {
+    dist: './dist/css/images',
+    live: './live/css/images'
+  }
+}
+
+
+/* SCRIPTS ------------------------ */
 // JSHint task
 gulp.task('jshint', function() {
   gulp.src('./src/js/*.js')
@@ -24,106 +47,85 @@ gulp.task('jshint', function() {
     .pipe(jshint.reporter('default'));
 });
 
-// JSHint watch task
-gulp.task('jsWatch', function() {
-  // watch for JS changes
-  gulp.watch('./src/js/*.js', function() {
-    gulp.run('jshint');
-  });
-});
-
-// Sass task
-gulp.task('sass', function () {
-  gulp.src('./src/css/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./build/css/'));
-});
-
-// add prefixes and minify css
-gulp.task('styles', function() {
-  gulp.src(['./src/css/*.css'])
-    .pipe(concat('style.css'))
-    .pipe(autoprefix('last 2 versions'))
-    .pipe(minifyCSS())
-    .pipe(gulp.dest('./build/css/'));
-});
-
-// minify new images
-gulp.task('imagemin', function() {
-  var imgSrc = './src/css/images/*.{jpg,png}',
-      imgDst = './build/css/images';
-
-  gulp.src(imgSrc)
-    .pipe(changed(imgDst))
-    .pipe(imagemin())
-    .pipe(gulp.dest(imgDst));
-});
-
-// gzip css and js
-// BROKEN
-// gulp.task('gzip', function() {
-// 	var srcOne = './src/css/*.css',
-// 		dstOne = './build/css/',
-// 		srcTwo = './src/js/*.js',
-// 		dstTwo = './build/js/';
-
-
-// 	gulp.src(srcOne)
-// 		.pipe(gzip({ append:false }))
-// 		.pipe(gulp.dest(dstOne));
-// 	gulp.src(srcTwo)
-// 		.pipe(gzip({ append:false }))
-// 		.pipe(gulp.dest(dstTwo));
-// });
-
-// minify html
-gulp.task('minifyHTML', function() {
-	var htmlSrc = './src/*.html',
-			htmlDst = './build';
-
-	gulp.src(htmlSrc)
-		.pipe(changed(htmlDst))
-		.pipe(minifyHTML())
-		.pipe(gulp.dest(htmlDst));
-})
-
 // JS concat, strip debugging and minify
 gulp.task('scripts', function() {
   gulp.src('./src/js/*.js')
     .pipe(concat('main.js'))
     .pipe(stripDebug())
     .pipe(uglify())
-    .pipe(gulp.dest('./build/js/'));
+    .pipe(gulp.dest(dir.js.dist))
+    .pipe(gulp.dest(dir.js.live));
 });
 
-// Build out all files that haven't yet been built
-gulp.task('buildAll', function() {
-  var src = ['./src/*', './src/**/*'],
-      dst = './build/';
 
-  gulp.src(src)
-    .pipe(changed(dst))
-    .pipe(gulp.dest(dst));
+/* CSS ------------------------ */
+// add prefixes and minify css
+gulp.task('styles', function() {
+  gulp.src(['./src/css/*.css'])
+    .pipe(concat('style.css'))
+    .pipe(autoprefix('last 2 versions'))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest(dir.css.dist))
+    .pipe(gulp.dest(dir.css.live));
+});
+
+
+/* ASSETS --------------------- */
+// minify new images
+gulp.task('imagemin', function() {
+  gulp.src('./src/css/images/*.{jpg,png}')
+    .pipe(changed(dir.img.dist))
+    .pipe(imagemin())
+    .pipe(gulp.dest(dir.img.dist))
+    .pipe(gulp.dest(dir.img.live));
+});
+
+
+/* HTML ------------------------ */
+// minify html
+gulp.task('minifyHTML', function() {
+	gulp.src('./src/*.html')
+		.pipe(changed(dir.root.dist))
+		.pipe(minifyHTML())
+		.pipe(gulp.dest(dir.root.dist))
+    .pipe(gulp.dest(dir.root.live));
+})
+
+// Build out all files that haven't yet been built
+gulp.task('build', function() {
+  var src = ['./src/*', './src/**/*'],
+      dest = makeDest();
+
+  gulp.src(['./src/*', './src/**/*'])
+    .pipe(changed(dir.root.dist))
+    .pipe(gulp.dest(dir.root.dist))
+    .pipe(gulp.dest(dir.root.live));
 });
 
 // default task
-gulp.task('default', ['imagemin', 'minifyHTML', 'scripts', 'styles']);
+gulp.task('default', ['minifyHTML', 'scripts', 'styles', 'imagemin']);
 
 // watch task
 gulp.task('watch', function() {
-  // watch for HTML changes
+
+  // HTML
   gulp.watch('./src/*.html', function() {
     gulp.run('minifyHTML');
   });
 
-  // watch for JS changes
-  gulp.watch('./src/js/*.js', function() {
-    gulp.run('scripts');
-  });
-
-  // watch for CSS changes
+  // CSS
   gulp.watch('./src/css/*.css', function() {
     gulp.run('styles');
   });
-});
 
+  // JS
+  gulp.watch('./src/js/*.js', function() {
+    gulp.run('scripts');
+    gulp.run('jshint');
+  });
+
+  // Build
+  gulp.watch(['./src/*', './src/**/*'], function() {
+    gulp.run('build');
+  });
+});
