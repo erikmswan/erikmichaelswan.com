568bada75942a460232e5ba008be819d5a81b267
Erik Michael Swan <erikmswan@gmail.com>
2016-05-21 13:08:25 -0700

    added cateachers portfolio item

--- js/main.js
+++ js/main.js
@@ -1,1 +1,53 @@
-function makeScene(e,l){var o={triggerElement:e};if(null!==l&&"object"==typeof l)for(var n in o)o[n]=l[n];return new ScrollMagic.Scene(o)}var nav=$(".main-nav");$(window).scroll(function(){$(this).scrollTop()>1?(nav.addClass("scroll-nav"),$(".big-title").hide(),$(".scroll-title-container").show()):(nav.removeClass("scroll-nav"),$(".big-title").show(),$(".scroll-title-container").hide())});var scrollControl=new ScrollMagic.Controller;makeScene("#intro").setClassToggle("#intro .content","fade-up").addTo(scrollControl),makeScene(".berkeley").setClassToggle(".berkeley","fade-left").addTo(scrollControl),makeScene(".cambridge").setClassToggle(".cambridge","fade-left").addTo(scrollControl);
+
+// MAIN.JS ----------------------------------*/
+
+// Scroll-nav class switch
+var nav = $('.main-nav');
+
+$(window).scroll(function () {
+	if ($(this).scrollTop() > 1) {
+		nav.addClass("scroll-nav");
+		$('.big-title').hide();
+		$('.scroll-title-container').show();
+	} else {
+		nav.removeClass("scroll-nav");
+		$('.big-title').show();
+		$('.scroll-title-container').hide();
+	}
+});
+
+// SCROLLMAGIC ----------------------------------*/
+var scrollControl = new ScrollMagic.Controller();
+
+// Scene maker function
+function makeScene(triggerEl, extraOptions) {
+	var options = { triggerElement: triggerEl };
+	if (extraOptions !== null && typeof extraOptions === 'object') {
+		for (var key in options) {
+			options[key] = extraOptions[key];
+		}
+	}
+	return new ScrollMagic.Scene(options);
+}
+
+// Scroll Animations
+makeScene('#intro')
+	.setClassToggle('#intro .content', 'fade-up')
+	.addTo(scrollControl);
+makeScene('.berkeley')
+	.setClassToggle('.berkeley', 'fade-left')
+	.addTo(scrollControl);
+makeScene('.berkeley')
+	.setClassToggle('.cambridge', 'fade-left')
+	.addTo(scrollControl);
+
+// // Animation on scroll
+// $(window).scroll(function() {
+// 	var scroll = $(window).scrollTop();
+//
+//  	scroll >= 250 ? $(".intro-wrap").addClass("fade-up") : $(".intro-wrap").removeClass("fade-up");
+// 	// Berkeley reveal
+// 	scroll >= 1500 ? $(".berkeley").addClass("fade-left") : $(".berkeley").removeClass("fade-left");
+// 	// Cambridge Reveal
+// 	scroll >= 1700 ? $(".cambridge").addClass("fade-right") : $(".cambridge").removeClass("fade-right");
+// });
